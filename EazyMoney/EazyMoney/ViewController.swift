dac63b3647c58680af4730c73277ed8d44e63717
Utkarsh Sharma <skhiearth@gmail.com>
2020-11-29 02:00:57 +0530

    app init

---
+++ EazyMoney/EazyMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EazyMoney
+//
+//  Created by Utkarsh Sharma on 29/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
